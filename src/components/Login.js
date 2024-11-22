b25f31186093e52e45f80d1ab6a7e0739d5ebfec
mbulic97 <mirnesbulic32@gmail.com>
2024-11-22 23:11:47 +0100

    Create State and Context API

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from "../images/logo.png";
 import {Link} from "react-router-dom";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {auth} from "../firebase";
 import "./Login.css"
+import { useAuth } from '../context/GlobalState';
 export const Login = () => {
+  const {user} = useAuth();
+  const [email,setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const register = (e)=>{
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password);
+  };
+  console.log(user);
   return (
     <div className='login'>
       <Link to="/">
@@ -12,7 +23,14 @@ export const Login = () => {
         <h1>Sign in</h1>
         <form>
           <h5>Email</h5>
-          <input type="password" value=""/>
+          <input 
+          type='email'
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}/>
+          <h5>Password</h5>
+          <input type="password"
+          value={password}
+           onChange={(e)=> setPassword(e.target.value)}/>
           <button className='login-signInBtn' type="submit">
             Sign in
           </button>
@@ -20,7 +38,7 @@ export const Login = () => {
           By continuing, you agree to Amazon's Fake Clone Conditions of Use
           and Privacy Notice.
           </p>
-          <button className='login-registerBtn'>
+          <button className='login-registerBtn' onClick={register}>
             Create your Amazon Account
           </button>
         </form>
